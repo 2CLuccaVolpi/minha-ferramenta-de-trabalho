af8413a02a40ce2f67a308a4dc7e5811a8407242
2CLuccaVolpi <0000110378660XSP@al.educacao.sp.gov.br>
2024-11-08 11:40:53 -0300

    Add files via upload

---
+++ minha ferramenta de estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Roblox?',
+    'O roblox é uma plataforma criar e jogar'
+)
+
+criaCartao(
+    'História',
+    'Quando se encerrou a segunda guerra mundial',
+    'Ela se encerrou em 1945'
+)
+
+criaCartao(
+    'Jogando um jogo estressante tipo fifa',
+    'O que é o fifa?',
+    'Um jogo de futebol que se estressa muito'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
